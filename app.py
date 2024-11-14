de551bab1c5c75c1bdc88ec5afc37f9dc3cb3928
Hank Helmers <89431423+HankHelmers@users.noreply.github.com>
2024-11-14 08:59:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 import time
 from bs4 import BeautifulSoup
@@ -33,7 +36,95 @@ def get_website_content(url):
         if driver is not None: driver.quit()
     return None
 
+## Click transcriptDNA taking to full info page
+def clickTranscriptDNA(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
 
+        time.sleep(5)
+
+        print('Page Loaded!')
+
+        # Look for element
+        element = driver.find_element(By.XPATH, '//*[@id="track_Transcripts"]/canvas')
+        element.click()
+        return driver.current_url
+
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
+    
+
+def getGeneName(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+
+        element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="content"]/div/div[1]/dl/dd[1]'))
+        )
+        print(element.text)
+
+        name = element.text
+        print(name.split("\n"))  # Splits into an array of elements each split where there was an '\n', in this case [header], [sequence]
+        return name.split("\n")[0]
+
+
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
+
+def getCDS(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+
+        ## Click button to go to Blast
+        element = WebDriverWait(driver, 15).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div/div[4]/div[4]/label/div/div/a/button/span'))
+        )
+        element.click()
+        
+        element = WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="content"]/div/div/div/div/form/fieldset/textarea'))
+        )
+        sequence = element.text
+        return sequence.split("\n")[1]
+
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
 
 
 # ---------------- Page & UI/UX Components ------------------------
@@ -44,15 +135,26 @@ def main_sidebar():
 
 
 def site_extraction_page():
-    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    SAMPLE_URL = "https://phytozome-next.jgi.doe.gov/jbrowse/index.html?data=genomes%2FTaestivumcv_ChineseSpring_v2_1&loc=Chr2B%3A646472438..646474664&tracks=UserBlastResults%2CTranscripts%2CAlt_Transcripts%2CPASA_assembly%2CBlastx_protein&highlight="
     url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
 
     clicked = st.button("Load Page Content",type="primary")
     if clicked:
         with st.container(border=True):
             with st.spinner("Loading page website..."):
-                content = get_website_content(url)
-                st.write(content)
+                # content = get_website_content(url)
+                # st.write(content)
+                
+                nextUrl = clickTranscriptDNA(url)
+                st.write(nextUrl)
+
+                geneName = getGeneName(nextUrl)
+                st.write(geneName)
+
+                cdsText = getCDS(nextUrl)
+                st.write(cdsText)
+
+
 
 
 if __name__ == "__main__":
